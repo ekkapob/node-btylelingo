3d2cbcc693043bbad6eb23df761d0f640ddcb0cd
Ekkapob <ekkapob@gmail.com>
2016-02-15 18:47:39 +0700

    add logo

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,21 @@ gulp.task('serve', function(){
   browserSync.init({
     proxy: "localhost:8080"
   });
-  gulp.watch("public/views/**/*.html").on('change', browserSync.reload);
+  gulp.watch("public/views/*.html").on('change', browserSync.reload);
+  gulp.watch("public/assets/css/*.css").on('change', browserSync.reload);
 });
 
-// gulp.task('server', function(){
-//   nodemon({
-//     script: 'server.js'
-//   });
-// });
+gulp.task('server', function(){
+  nodemon({
+    script: 'server.js',
+    ext: 'js public/views/partials/*.html'
+  }).on('restart', function(){
+    setTimeout(browserSync.reload, 500);
+  });
 
-gulp.task('default', ['serve']);
+  setTimeout(function(){
+    gulp.run('serve');
+  }, 500);
+});
+
+gulp.task('default', ['server']);
