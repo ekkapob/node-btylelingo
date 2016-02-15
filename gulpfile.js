7d53f3edd5d99e8474bc7b1f00c75bbf0017e6db
Ekkapob C <ekkapob@gmail.com>
2016-02-16 00:19:04 +0700

    sass and setups

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,28 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var nodemon     = require('gulp-nodemon');
-var runSequence = require('run-sequence');
+var concat      = require('gulp-concat');
+var sourcemaps  = require('gulp-sourcemaps');
+
+gulp.task('sass', function(){
+  return gulp.src('public/assets/scss/*.scss')
+          .pipe(sourcemaps.init())
+          .pipe(sass().on('error', sass.logError))
+          .pipe(concat('app.css'))
+          .pipe(sourcemaps.write())
+          .pipe(gulp.dest('public/assets/css'));
+});
 
 gulp.task('serve', function(){
   browserSync.init({
-    proxy: "localhost:8080"
+    proxy: 'localhost:8080'
   });
-  gulp.watch("public/views/*.html").on('change', browserSync.reload);
-  gulp.watch("public/assets/css/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('watch', function(){
+  gulp.watch('public/views/*.html').on('change', browserSync.reload);
+  gulp.watch('public/assets/css/*.css').on('change', browserSync.reload);
+  gulp.watch('public/assets/scss/*.scss', ['sass']);
 });
 
 gulp.task('server', function(){
@@ -25,4 +39,4 @@ gulp.task('server', function(){
   }, 500);
 });
 
-gulp.task('default', ['server']);
+gulp.task('default', ['server', 'watch']);
